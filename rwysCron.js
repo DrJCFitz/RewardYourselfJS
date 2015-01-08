230c3fa5a7c0a0b3c7449f7e056c77c42d0ab27e
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-01-07 20:17:43 -0500

    modified model for matching reward regex

--- rwysCron.js
+++ rwysCron.js
@@ -4,14 +4,14 @@ try {
     var Spooky = require('../lib/spooky');
 }
 var $ = require('jquery');
-var portals = require('./config/portals');
+var portals = require('./config/portals.js');
 var mongoClient = require('mongodb').MongoClient;
 var spookyConfig = 
   { child: { transport: 'http' },
     casper: { logLevel: 'debug',
             verbose: true, 
-            clientScripts: [ 'js/jquery2.1.3.js',
-                'js/pageScrape.js'
+            clientScripts: [ 'assets/js/jquery2.1.3.js',
+                'assets/js/pageScrape.js'
               ],
             pageSettings: {
                 javascriptEnabled: true,
@@ -33,7 +33,7 @@ var spookyFunction = function (err) {
         throw e;
     }
     //spooky.start(portals[1].portal.baseUrl + portals[1].portal.storePath);
-    spooky.start(portals[0].portal.baseurl+portals[0].portal.storePath);
+   console.log(portals[0].portal.baseUrl); spooky.start('http://localhost:3000'+portals[0].portal.storePath);
     spooky.then( [{port:portals[0]},
         function(){ 
             this.emit('processedMerchant',
@@ -82,7 +82,7 @@ spooky.on('storeName', function (name) {
 });
 
 spooky.on('processedMerchant', function (result) {
-    mongoClient.connect('mongodb://127.0.0.1:27107/merchant',
+    mongoClient.connect('mongodb://127.0.0.1:27017/merchant',
         function(err, db) {
             if(err) throw err;
             var collection = db.collection('merchant');
