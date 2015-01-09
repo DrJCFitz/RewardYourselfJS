934946ebee86b92f075b0b7fbc688e1bda725254
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-01-09 07:54:17 -0500

    implement EmberJS front-end framework

--- rewardYourself.js
+++ rewardYourself.js
@@ -4,17 +4,31 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 //var mongo = require('mongo');
-//var monk = require('monk');
-//var db = monk('localhost:27017/merchant');
-
+var monk = require('monk');
+var db = monk('localhost:27017/merchant');
+var merchant = db.get('merchant');
 var portnumber = 3000;
 
 var app = express();
 // Serve static files
-app.use('/assets',express.static(__dirname+'/assets'));
+app.use('/assets',express.static(__dirname+'/dist/assets'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname+'/views/rwys.html');
+    res.sendFile(__dirname+'/dist/index.html');
+    //res.sendFile(__dirname+'/views/rwys.html');
+});
+
+app.get('/stores', function(req, res) {
+    merchant.find({},{}, function(err, foundMerchant){
+        res.send(foundMerchant);
+    });
+});
+
+app.get('/online/stores/:mkey', function(req, res) {
+    console.log(req.params.mkey);
+    merchant.find({key: req.params.mkey, enabled: true, type: 'online'}, {}, function(err, foundMerchant){
+        res.send(foundMerchant);
+    });
 });
 
 app.listen(portnumber);
