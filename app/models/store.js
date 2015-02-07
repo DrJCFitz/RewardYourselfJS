66723d176e8ae679f257d35dba0f7c0902d0d115
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-02-07 14:46:33 -0500

    merge mobile-responsive changes

--- app/models/store.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  portalName: DS.attr('string'),
-  portalKey: DS.attr('string'),
-  link: DS.attr('string'),
-  reward: DS.belongsTo('reward', {embedded: 'always'})
-});
