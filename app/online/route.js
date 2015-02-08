310f7fa1375c0c9f376923ff502af406fb0f0f88
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-02-07 19:03:21 -0500

    put data in fixtures

--- app/online/route.js
+++ app/online/route.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        var thisModel = this;
-        return new Ember.RSVP.Promise(function(resolve){
-            setTimeout(resolve,1000);
-        }).then(function() {
-            return thisModel.store.find('store');
-        });
+        return this.store.find('store');
     }
 });
--- app/store/model.js
+++ app/store/model.js
@@ -1,9 +1,17 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Store = DS.Model.extend({
   name: DS.attr('string'),
   portalName: DS.attr('string'),
   portalKey: DS.attr('string'),
   link: DS.attr('string'),
-  reward: DS.belongsTo('reward', {embedded: 'always'})
+  reward: DS.belongsTo('reward')
 });
+
+Store.reopenClass({
+    FIXTURES: [
+        { id: '243dade', name: 'Store1', portalName: 'Ebates', portalKey: 'ebates', link: 'http://cnn.com', reward: 'ebates1234' }
+    ]
+});
+
+export default Store;
