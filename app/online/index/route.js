e1d9732c89ec3aa01aad5ac10dc7037175802ba7
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-03-24 07:23:57 -0400

    removing separated routes

--- app/online/index/route.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-    model: function() {
-        return new Ember.RSVP.Promise(function(resolve){
-            setTimeout(resolve,3000);
-        });
-    }
-});
