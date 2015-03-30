b085d7fda8641fa3fb46dd1a19920d063bda64f8
John Fitzpatrick <john.c.fitzpatrick.dr@gmail.com>
2015-03-30 12:26:42 -0400

    implement top deals components

--- app/offers/offer/view.js
+++ app/offers/offer/view.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.View.extend({
     templateName: 'offers/offer',
     tagName: 'tr',
-    classNames: ['offer-instance'],
+    classNames: ['offerInstance'],
     click: function() {
         window.open(this.get('content').get('link'));
     }
